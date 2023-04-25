060d8448288e603b9285564e73e6440f109cafa5
Amitd <amit.dankner@cyberbit.com>
2023-04-25 16:18:34 +0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-
+import csv
+import subprocess
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, make_response)
 
 app = Flask(__name__)
 
@@ -27,6 +28,13 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+@app.route('/download')
+def download_file():
+    # Run the Git clone command using subprocess
+    subprocess.run(['git', 'clone', 'https://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@github.com/dankner23/IT.git'])
+
+    # Return a success message to the user
+    return 'Repository cloned successfully!'
 
 if __name__ == '__main__':
    app.run()
